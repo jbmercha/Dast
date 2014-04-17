638f0867263601d52d25dcffec4c47ccb1bbfc2a
jbmercha <jbmercha@gmail.com>
2014-04-17 16:16:17 -0700

    Adding hast table with chainging

---
+++ Dast/HashTableWithChaining.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dast
+{
+    public class HashTableWithChaining<TKey, TValue> : IHashTable<TKey, TValue>
+    {
+        private struct HashEntry
+        {
+            public TKey Key;
+            public List<TValue> ValueList;
+        }
+        private EqualityComparer<TKey> _comparer;
+
+        private HashEntry[] _array;
+
+        public HashTableWithChaining()
+        {
+            Count = 0;
+            Capacity = 32;
+            _comparer = EqualityComparer<TKey>.Default;
+            _array = new HashEntry[Capacity];
+        }
+
+        public int Capacity { get; private set; }
+
+        public int Count { get; private set; }
+
+        public TValue this[TKey key]
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+
+        public int HashKey(TKey key)
+        {
+            return _comparer.GetHashCode(key) % Capacity;
+        }
+           
+
+        public void Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
