1f9e5a040938ce32e60d6d1808cbb6041fac2ecc
jbmercha <jbmercha@gmail.com>
2014-05-19 14:58:24 -0700

    initial queue

---
+++ Dast/Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dast
+{
+
+    public interface IQueue<T>
+    {
+        int Count { get; }
+        void Enqueue(T item);
+        T Dequeue();
+    }
+
+    public class CircularBufferQueue<T> : IQueue<T>
+    {
+        private T[] _array;
+        private int _capacity;
+        private int _start;
+        private int _end;
+        public CircularBufferQueue()
+        {
+            _capacity = 4;
+            _array = new T[_capacity];
+            _start = 0;
+            _end = 0;
+        }
+
+        public int Count { get; private set; }
+        
+        public void Enqueue(T item)
+        {
+            if (Count == _capacity)
+                GrowArray();
+
+            _array[_end] = item;
+            _end = (_end + 1) % _capacity;
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if(Count == 0)
+                throw new Exception("Queue is empty. Cannot Dequeue");
+            var item = _array[_start];
+            _array[_start] = default (T);
+            _start = (_start + 1) % _capacity;
+            Count--;
+            return item;
+        }
+
+        private void GrowArray()
+        {
+            _capacity *= 2;
+            var newArray = new T[_capacity];
+            
+            Array.Copy(_array, _start, newArray, 0, _array.Length - _start);
+            Array.Copy(_array, 0, newArray, _array.Length - _start, _start);
+            _end = Count;
+            _start = 0;
+            _array = newArray;
+        }
+    }
+}
---
+++ DastUnitTests/QueueUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dast;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DastUnitTests
+{
+    [TestClass]
+    public class QueueUnitTests
+    {
+
+        [TestMethod]
+        public void CircularBufferQueueBasicRandomOperations()
+        {
+            RandomOperations(new CircularBufferQueue<int>());
+        }
+
+        public void RandomOperations(IQueue<int> target)
+        {
+            var r = new Random();
+            var frameworkQueue = new Queue<int>();
+            var operations = Enumerable.Range(0, 100000).Select(x => r.Next(2)).ToArray();
+            foreach (var operation in operations)
+            {
+                int oldCount = target.Count;
+                switch (operation)
+                {
+                    case 0: // Add
+                        var newItem = r.Next();
+                        target.Enqueue(newItem);
+                        frameworkQueue.Enqueue(newItem);
+                        Assert.AreEqual(oldCount + 1, target.Count);
+                        break;
+                    case 1: // Remove by index
+                        if (oldCount == 0) goto case 0;
+                        var item = target.Dequeue();
+                        var frameworkItem = frameworkQueue.Dequeue();
+                        Assert.AreEqual(oldCount - 1, target.Count);
+                        Assert.AreEqual(frameworkItem, item);
+                        break;
+                }
+            }
+        }
+    }
+}
