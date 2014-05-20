c3d5dc58387bede058b3b9ad48e80ccf24082791
jbmercha <jbmercha@gmail.com>
2014-05-20 07:59:52 -0700

    Stack add new files

---
+++ Dast/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Dast
+{
+
+    public interface IStack<T>
+    {
+        int Count { get; }
+        void Push(T item);
+        T Pop();
+    }
+
+
+    public class ArrayStack<T> : IStack<T>
+    {
+        private T[] _array;
+        private int _capacity;
+
+        public ArrayStack()
+        {
+            _capacity = 4;
+            _array = new T[_capacity];
+        }
+
+        public int Count { get; private set; }
+        public void Push(T item)
+        {
+            if (Count == _capacity)
+                GrowArray();
+            _array[Count] = item;
+            Count++;
+        }
+
+        private void GrowArray()
+        {
+            _capacity *= 2;
+            var newArray = new T[_capacity];
+            Array.Copy(_array, 0, newArray, 0, _array.Length);
+            _array = newArray;
+        }
+
+        public T Pop()
+        {
+            if(Count == 0)
+                throw new Exception("Stack is empty");
+            Count--;
+            var item = _array[Count];
+            _array[Count] = default (T);
+            return item;
+        }
+    }
+}
---
+++ DastUnitTests/StackUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dast;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DastUnitTests
+{
+    [TestClass]
+    public class StackUnitTests
+    {
+        [TestMethod]
+        public void ArrayStackRandomOperations()
+        {
+            var target = new ArrayStack<int>();
+            RandomOperations(target);
+        }
+
+       
+        public void RandomOperations(IStack<int> target)
+        {
+            var r = new Random();
+            var frameworkStack = new Stack<int>();
+            var operations = Enumerable.Range(0, 100000).Select(x => r.Next(2)).ToArray();
+            foreach (var operation in operations)
+            {
+                int oldCount = target.Count;
+                switch (operation)
+                {
+                    case 0: // Add
+                        var newItem = r.Next();
+                        target.Push(newItem);
+                        frameworkStack.Push(newItem);
+                        Assert.AreEqual(oldCount + 1, target.Count);
+                        break;
+                    case 1: // Remove
+                        if (oldCount == 0) goto case 0;
+                        var item = target.Pop();
+                        var frameworkItem = frameworkStack.Pop();
+                        Assert.AreEqual(oldCount - 1, target.Count);
+                        Assert.AreEqual(frameworkItem, item);
+                        break;
+                }
+            }
+        }
+    }
+}
